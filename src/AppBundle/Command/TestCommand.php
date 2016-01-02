95440afee5e3ea859de8dc720d134768532bcefb
Mikaël Labrut <labrut@gmail.com>
2016-01-02 16:32:01 +0100

    wip

--- src/AppBundle/Command/TestCommand.php
+++ src/AppBundle/Command/TestCommand.php
@@ -27,19 +27,19 @@ class TestCommand extends ContainerAwareCommand
         $rootPath = $this->getContainer()->get('kernel')->getRootDir().'/../';
         require_once($rootPath.'vendor/mike42/escpos-php/Escpos.php');
 
-        $imagePath = $rootPath.'resources/elephant-70x70.png';
+        $imagePath = $rootPath.'resources/elephant-256x256.png';
         $img = new \EscposImage($imagePath);
 
         $connector = new \FilePrintConnector('/dev/usb/lp0');
         $printer = new \Escpos($connector);
         $printer->initialize();
 
-        $printer->text('*************************');
-        $printer->text('Un petit elephant pour tester !');
+        $printer->text('*************************\n');
+        $printer->text('Un petit elephant');
         $printer->feed(2);
-        $printer->bitImage($img, \Escpos::IMG_DOUBLE_WIDTH | \Escpos::IMG_DOUBLE_HEIGHT);
+        $printer->bitImage($img);
         $printer->feed(2);
-        $printer->text('*************************');
+        $printer->text('*************************\n');
 
         /* Always close the printer! On some PrintConnectors, no actual
          * data is sent until the printer is closed. */
