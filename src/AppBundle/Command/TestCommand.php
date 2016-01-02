aaf30f196c0a0fa17c314ea2d6aef02ed2a67277
Mikaël Labrut <labrut@gmail.com>
2016-01-02 15:08:01 +0100

    wip

---
+++ src/AppBundle/Command/TestCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TestCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('test:test')
+            ->setDescription('Test')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $rootPath = $this->getContainer()->get('kernel')->getRootDir().'/../';
+
+        $imagePath = $rootPath.'resources/elephant-70x70.png';
+        $img = new \EscposImage($imagePath);
+
+        $connector = new \FilePrintConnector('/dev/usb/lp0');
+        $printer = new \Escpos($connector);
+        $printer->initialize();
+
+        $printer->text('Elephant test !');
+        $printer->feed(7);
+
+        /* Graphics - this demo will not work on some non-Epson printers */
+        try {
+            $printer->graphics($img, \Escpos::IMG_DEFAULT);
+        } catch (\Exception $e) {
+            /* Images not supported on your PHP, or image file not found */
+            $printer->text($e->getMessage()."\n");
+        }
+        $printer->cut();
+
+        /* Bit image */
+        try {
+            $printer->bitImage($img, \Escpos::IMG_DEFAULT);
+        } catch (\Exception $e) {
+            /* Images not supported on your PHP, or image file not found */
+            $printer->text($e->getMessage()."\n");
+        }
+        $printer->cut();
+
+        /* Pulse */
+        $printer->pulse();
+
+        /* Always close the printer! On some PrintConnectors, no actual
+         * data is sent until the printer is closed. */
+        $printer->close();
+
+        return 0;
+    }
+}
