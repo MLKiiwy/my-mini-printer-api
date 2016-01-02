bf791427879f51ff030541416442df56ac21d852
Mikaël Labrut <labrut@gmail.com>
2016-01-02 16:09:58 +0100

    wip

--- src/AppBundle/Command/TestCommand.php
+++ src/AppBundle/Command/TestCommand.php
@@ -30,37 +30,22 @@ class TestCommand extends ContainerAwareCommand
         $imagePath = $rootPath.'resources/elephant-70x70.png';
         $img = new \EscposImage($imagePath);
 
-        //$connector = new \FilePrintConnector('/home/pi/test');
+        //$connector = new \FilePrintConnector('~/test.log');
         $connector = new \FilePrintConnector('/dev/usb/lp0');
         $printer = new \Escpos($connector);
         $printer->initialize();
 
         $printer->text('Elephant test !');
-        $printer->feed(1);
+        $printer->feed(2);
 
-        /* Graphics - this demo will not work on some non-Epson printers */
         try {
-//            $printer->graphics($img, \Escpos::IMG_DOUBLE_WIDTH | \Escpos::IMG_DOUBLE_HEIGHT);
+            $printer->graphics($img, \Escpos::IMG_DEFAULT);
+            //$printer->bitImage($img, \Escpos::IMG_DEFAULT);
         } catch (\Exception $e) {
             /* Images not supported on your PHP, or image file not found */
             echo $e;
-		$printer->text($e->getMessage()."\n");
         }
-$printer->feed(1);
-        $printer->cut();
-
-        /* Bit image */
-        try {
-            $printer->bitImage($img, \Escpos::IMG_DEFAULT);
-        } catch (\Exception $e) {
-            echo $e;
-		/* Images not supported on your PHP, or image file not found */
-            $printer->text($e->getMessage()."\n");
-        }
-//$printer->feed(1);
-  //      $printer->cut();
-
-
+        $printer->feed(10);
 
         /* Always close the printer! On some PrintConnectors, no actual
          * data is sent until the printer is closed. */
